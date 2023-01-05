114ed6ef12837db7b46bc87dde4d8bc101d96487
Olaniyi Olabode <Olaniyiolabode99@yahoo.com>
2023-01-05 23:03:28 +0100

    filtersection resuable

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,7 +1,7 @@
+import imageFour from "../assets/listing_img_four.png";
 import imageOne from "../assets/listing_img_one.png";
 import imageTwo from "../assets/listing_img_three.png";
 import imageThree from "../assets/listing_img_two.png";
-import imageFour from "../assets/listing_img_four.png";
 import imageFive from "../assets/recent_listing.png";
 export const apartmentData = [
   {
@@ -152,5 +152,126 @@ export const cartHomeData = [
     apartmentImage: imageFive,
     apartmentName: "Luxury duplex With Terrace",
     apartmentPrice: "34,900",
+  },
+];
+
+export const filterTypeOfAccomodationData = [
+  {
+    id: 1,
+    name: "All",
+    value: "All",
+  },
+  {
+    id: 2,
+    name: "Apartment",
+    value: "Apartment",
+  },
+  {
+    id: 3,
+    name: "House",
+    value: "House",
+  },
+  {
+    id: 4,
+    name: "Villa",
+    value: "Villa",
+  },
+  {
+    id: 5,
+    name: "Office",
+    value: "Office",
+  },
+];
+
+export const numberOfBathroomsData = [
+  {
+    id: 1,
+  },
+  {
+    id: 2,
+  },
+  {
+    id: 3,
+  },
+  {
+    id: 4,
+  },
+  {
+    id: 5,
+  },
+];
+
+export const mainFeaturesData = [
+  {
+    id: 1,
+    name: "Television",
+  },
+  {
+    id: 2,
+    name: "Swimming Pool",
+  },
+  {
+    id: 3,
+    name: "Microwave",
+  },
+  {
+    id: 4,
+    name: "Air Conditioning",
+  },
+  {
+    id: 5,
+    name: "Parking",
+  },
+  {
+    id: 6,
+    name: "Heater",
+  },
+  {
+    id: 7,
+    name: "Washing Machine",
+  },
+  {
+    id: 8,
+    name: "Balcony",
+  },
+  {
+    id: 9,
+    name: "Wifi",
+  },
+  {
+    id: 10,
+    name: "Dishwasher",
+  },
+  {
+    id: 11,
+    name: "Garage",
+  },
+  {
+    id: 12,
+    name: "Satellite Tv",
+  },
+  {
+    id: 13,
+    name: "Terrace",
+  },
+];
+
+export const budgetData = [
+  {
+    id: 1,
+    name: "Low Budget",
+    price1: "10,000",
+    price2: "30,000",
+  },
+  {
+    id: 2,
+    name: "Medium Budget",
+    price1: "10,000",
+    price2: "30,000",
+  },
+  {
+    id: 3,
+    name: "High Budget",
+    price1: "30,000 Up",
   },
 ];
