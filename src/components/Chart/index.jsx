7631cf1f15881fe5b51b6f411c172e9822f3f029
Akinyemi Bamidele <59539644+bamidele2161@users.noreply.github.com>
2023-03-02 18:06:59 +0100

    worked on check in bar chart

--- src/components/Chart/index.jsx
+++ src/components/Chart/index.jsx
@@ -1,4 +1,14 @@
-import React from "react";
+import {
+  eachWeekOfInterval,
+  isSameDay,
+  isSameWeek,
+  parseJSON,
+  subMonths,
+  subWeeks,
+} from "date-fns";
+import { eachDayOfInterval, format } from "date-fns/esm";
+import React, { useEffect, useState } from "react";
+import { useCallback } from "react";
 import {
   BarChart,
   CartesianGrid,
@@ -12,60 +22,33 @@ import {
 import {
   useGetAllApartmentsQuery,
   useGetAllBookingsQuery,
+  useGetAllCheckInsQuery,
   useGetAllReservationsQuery,
 } from "../../store/Services/staffService";
+import {
+  getLastMonthData,
+  getLastWeekData,
+  getUniqueListBy,
+  mergedChartData,
+} from "../../utils/helper";
 import { ChartContainer, TopContent } from "./style";
 
 const CheckInBarChart = () => {
   //endpoints
-  const allApartments = useGetAllApartmentsQuery();
-  const getAllReservations = useGetAllReservationsQuery();
-  const getAllBookings = useGetAllBookingsQuery();
 
-  const data = [
-    {
-      name: "Mon",
-      Reservation: 400,
-      Bookings: 240,
-      // Apartment: 230,
-    },
-    {
-      name: "Tue",
-      Reservation: 300,
-      Bookings: 139,
-      // Apartment: 230,
-    },
-    {
-      name: "Wed",
-      Reservation: 200,
-      Bookings: 800,
-      // Apartment: 230,
-    },
-    {
-      name: "Thur",
-      Reservation: 278,
-      Bookings: 600,
-      // Apartment: 230,
-    },
-    {
-      name: "Fri",
-      Reservation: 189,
-      Bookings: 700,
-      // Apartment: 230,
-    },
-    {
-      name: "Sat",
-      Reservation: 239,
-      Bookings: 380,
-      // Apartment: 230,
-    },
-    {
-      name: "Sun",
-      Reservation: 349,
-      Bookings: 430,
-      // Apartment: 230,
-    },
-  ];
+  const getAllBookings = useGetAllBookingsQuery();
+  const [filterBy, setFilter] = useState("week");
+  const [chartData, setChartData] = useState([]);
+
+  const [checkInWeek, setCheckInWeek] = useState([]);
+
+  const [reserveWeek, setReserveWeek] = useState([]);
+
+  const [checkInMonth, setCheckInMonth] = useState([]);
+
+  const [reserveMonth, setReserveMonth] = useState([]);
+
+  const getAllCheckIns = useGetAllCheckInsQuery();
 
   const options = [
     {
@@ -77,6 +60,90 @@ const CheckInBarChart = () => {
       name: "Last Month",
     },
   ];
+  const getWeekData = useCallback(() => {
+    const today = new Date();
+    const lastWeek = subWeeks(today, 1);
+    const lastWeekData = getLastWeekData(getAllCheckIns?.data?.data[0]);
+    const everyday = eachDayOfInterval({ start: lastWeek, end: today });
+    return everyday.map((date) => {
+      return {
+        name: format(date, "E"),
+        checkin: lastWeekData.filter((data) =>
+          isSameDay(parseJSON(data.checkinDate), date)
+        ).length,
+      };
+    });
+  }, [getAllCheckIns]);
+
+  const getMonthData = useCallback(() => {
+    const today = new Date();
+    const lastMonth = subMonths(today, 1);
+    const lastMonthData = getLastMonthData(getAllCheckIns?.data?.data[0]);
+    const everyweek = eachWeekOfInterval({ start: lastMonth, end: today });
+    return everyweek.map((date) => {
+      return {
+        name: format(date, "MMM do"),
+        checkin: lastMonthData.filter((data) =>
+          isSameWeek(parseJSON(data.checkinDate), date)
+        ).length,
+      };
+    });
+  }, [getAllCheckIns]);
+
+  const getBookingWeekData = useCallback(() => {
+    const today = new Date();
+    const lastWeek = subWeeks(today, 1);
+    const lastWeekData = getLastWeekData(getAllBookings?.data?.data);
+    const everyday = eachDayOfInterval({ start: lastWeek, end: today });
+    return everyday.map((date) => {
+      return {
+        name: format(date, "E"),
+        booking: lastWeekData.filter((data) =>
+          isSameDay(parseJSON(data.datecreated), date)
+        ).length,
+      };
+    });
+  }, [getAllBookings]);
+
+  const getBookingMonthData = useCallback(() => {
+    const today = new Date();
+    const lastMonth = subMonths(today, 1);
+    const lastMonthData = getLastMonthData(getAllBookings?.data?.data);
+    const everyweek = eachWeekOfInterval({ start: lastMonth, end: today });
+    return everyweek.map((date) => {
+      return {
+        name: format(date, "MMM do"),
+        booking: lastMonthData.filter((data) =>
+          isSameWeek(parseJSON(data.datecreated), date)
+        ).length,
+      };
+    });
+  }, [getAllBookings]);
+
+  // const modify = () => {
+
+  // };
+  useEffect(() => {
+    setReserveWeek(getBookingWeekData);
+    setCheckInWeek(getWeekData);
+    setReserveMonth(getMonthData);
+    setCheckInMonth(getBookingMonthData);
+  }, [getWeekData, getMonthData, getBookingWeekData, getBookingMonthData]);
+
+  useEffect(() => {
+    if (filterBy === "week") {
+      let removecheckDuplicate = getUniqueListBy(checkInWeek, "name");
+      let removeReserveDuplicate = getUniqueListBy(reserveWeek, "name");
+      const merge = mergedChartData(
+        removecheckDuplicate,
+        removeReserveDuplicate
+      );
+      setChartData(merge);
+    } else {
+      const merge = mergedChartData(checkInMonth, reserveMonth);
+      setChartData(merge);
+    }
+  }, [filterBy, checkInWeek, reserveWeek, checkInMonth, reserveMonth]);
 
   return (
     <ChartContainer
@@ -89,8 +156,8 @@ const CheckInBarChart = () => {
         <h3>Check-Ins</h3>
         <select
           name="time"
-          //   onChange={(event) => setFilter(event.target.value)}
-          //   value={filterBy}
+          onChange={(event) => setFilter(event.target.value)}
+          value={filterBy}
         >
           {options.map((option, index) => (
             <option value={option.value} key={index}>
@@ -101,7 +168,7 @@ const CheckInBarChart = () => {
       </TopContent>
 
       <ResponsiveContainer width="100%" height="75%">
-        <BarChart width={730} height={250} data={data}>
+        <BarChart width={730} height={250} data={chartData}>
           <XAxis
             dataKey="name"
             tickLine={{ stroke: "none" }}
@@ -110,8 +177,8 @@ const CheckInBarChart = () => {
           <YAxis tickLine={{ stroke: "none" }} axisLine={{ stroke: "none" }} />
           <Tooltip />
           <Legend />
-          <Bar dataKey="Bookings" fill="#8BA00D" barSize={6} radius={20} />
-          <Bar dataKey="Reservation" fill="#FFCA2A" barSize={6} radius={20} />
+          <Bar dataKey="checkin" fill="#8BA00D" barSize={6} radius={20} />
+          <Bar dataKey="booking" fill="#FFCA2A" barSize={6} radius={20} />
           {/* <Bar dataKey="Apartment" fill="#C43C20" barSize={6} radius={20} /> */}
         </BarChart>
       </ResponsiveContainer>
--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,3 +1,5 @@
+import { isWithinInterval, subMonths, subWeeks } from "date-fns";
+
 export const calculateTotalPrice = (itemContainer) => {
   const totalPrice = itemContainer.reduce((accumulator, room) => {
     return accumulator + room.price * room.quantity;
@@ -53,4 +55,51 @@ export const convertToLink = async (image) => {
       console.error(error);
     });
   return await res;
+};
+
+export const getLastMonthData = (array) => {
+  const today = new Date(); // getting today's date
+  const priorMonth = subMonths(today, 1); //takes the date to be changed and the amount of months to be subtracted
+
+  array = array || [];
+  //checking if item date is in the array is withtin the interval
+  return array.filter((item) =>
+    isWithinInterval(new Date(item.checkinDate || item.datecreated), {
+      //the interval to check
+      start: priorMonth,
+      end: today,
+    })
+  );
+};
+
+export const getLastWeekData = (array) => {
+  const today = new Date(); // getting today's date
+  const priorWeek = subWeeks(today, 1); //takes the date to be changed and the amount of weeks to be subtracted
+
+  array = array || [];
+  //checking if item date is in the array is withtin the interval
+  return array.filter((item) =>
+    isWithinInterval(new Date(item.checkinDate || item.datecreated), {
+      //the interval to check
+      start: priorWeek,
+      end: today,
+    })
+  );
+};
+
+export const mergedChartData = (checkin, reservation) => {
+  let mergedList = [];
+  checkin?.forEach((checkin) => {
+    reservation?.forEach((reservation) => {
+      if (checkin.name === reservation.name) {
+        let newList = { ...checkin, ...reservation };
+        mergedList.push(newList);
+      }
+    });
+  });
+  return mergedList;
+};
+
+export const getUniqueListBy = (arr, key) => {
+  return [...new Map(arr.map((item) => [item[key], item])).values()];
 };
